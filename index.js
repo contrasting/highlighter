c82ab6648fafb79ec056f61f011ac042b6d511ed
fredchen <fredchen@economist.com>
2024-07-10 14:39:40 +0100

    specify wraparound search

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function applyHighlights(highlights) {
     // https://stackoverflow.com/questions/8276113/what-is-the-best-approach-to-search-some-text-in-body-html
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
     for (let h of highlights) {
-        window.find(h);
+        // wraparound otherwise will skip instances
+        window.find(h, true, false, true);
         let rng = window.getSelection().getRangeAt(0);
 
         const mark = document.createElement("mark");
