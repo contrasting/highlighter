562922a8d29a4722e769ba774ea3e662044114cc
fredchen <fredchen@economist.com>
2024-07-10 14:15:00 +0100

    add initial highlight

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 chrome.storage.local.onChanged.addListener((changes, namespace) => {
     const highlights = changes[window.location.href].newValue;
 
-    console.log(highlights);
+    // https://stackoverflow.com/questions/8276113/what-is-the-best-approach-to-search-some-text-in-body-html
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+    for (let h of highlights) {
+        window.find(h);
+        let rng = window.getSelection().getRangeAt(0);
+        rng.deleteContents();
+
+        const mark = document.createElement("mark");
+        const text = document.createTextNode(h);
+        mark.appendChild(text);
+
+        rng.insertNode(mark);
+        break;
+    }
 });
