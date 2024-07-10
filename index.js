d207ad38d3a2ead0a9972ca854d8a0a3bd5963e1
fredchen <fredchen@economist.com>
2024-07-10 14:43:02 +0100

    add undefined check

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // https://developer.chrome.com/docs/extensions/reference/api/storage#synchronous_response_to_storage_updates
-chrome.storage.local.onChanged.addListener((changes, namespace) => {
+chrome.storage.local.onChanged.addListener((changes) => {
     const highlights = changes[window.location.href].newValue;
     applyHighlights(highlights);
 });
 
 function applyHighlights(highlights) {
+    if (highlights === undefined) return;
+
     // https://stackoverflow.com/questions/8276113/what-is-the-best-approach-to-search-some-text-in-body-html
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
     for (let h of highlights) {
