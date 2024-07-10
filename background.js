155e4afaca923712f16733187ed1d6956aa74fb5
fredchen <fredchen@economist.com>
2024-07-10 12:37:34 +0100

    add highlights to storage

--- background.js
+++ background.js
@@ -9,7 +9,13 @@ chrome.runtime.onInstalled.addListener(async () => {
     });
 });
 
-chrome.contextMenus.onClicked.addListener((item, tab) => {
-    console.log(tab.url)
-    console.log(item.selectionText);
+chrome.contextMenus.onClicked.addListener(async (item, tab) => {
+    const url = tab.url;
+
+    // https://developer.chrome.com/docs/extensions/reference/api/storage#examples
+    const highlights = (await chrome.storage.local.get(url))[url] ?? [];
+
+    await chrome.storage.local.set({
+        [url]: [item.selectionText, ...highlights]
+    })
 });
