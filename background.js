d96db309f95fde4bd71d5bbf26c51267a6d32882
fredchen <fredchen@economist.com>
2024-07-11 11:45:38 +0100

    add export

--- background.js
+++ background.js
@@ -2,20 +2,42 @@
 
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.contextMenus.create({
-        id: "highlighter",
+        id: "highlighter_highlight",
         title: "Highlighter highlight",
         type: 'normal',
         contexts: ['selection']
     });
+    chrome.contextMenus.create({
+        id: "highlighter_export",
+        title: "Highlighter export",
+        type: "normal",
+        contexts: ["action"]
+    });
 });
 
 chrome.contextMenus.onClicked.addListener(async (item, tab) => {
-    const url = tab.url;
+    if (item.menuItemId === "highlighter_highlight") {
 
-    // https://developer.chrome.com/docs/extensions/reference/api/storage#examples
-    const highlights = (await chrome.storage.local.get(url))[url] ?? [];
+        const url = tab.url;
 
-    await chrome.storage.local.set({
-        [url]: [item.selectionText, ...highlights]
-    })
+        // https://developer.chrome.com/docs/extensions/reference/api/storage#examples
+        const highlights = (await chrome.storage.local.get(url))[url] ?? [];
+
+        await chrome.storage.local.set({
+            [url]: [item.selectionText, ...highlights]
+        })
+    } else if (item.menuItemId === "highlighter_export") {
+        // https://stackoverflow.com/questions/23160600/chrome-extension-local-storage-how-to-export
+        chrome.storage.local.get(null, function(items) { // null implies all items
+            // Convert object to a string.
+            const result = JSON.stringify(items);
+
+            // Save as file https://stackoverflow.com/questions/23223718/failed-to-execute-btoa-on-window-the-string-to-be-encoded-contains-characte
+            const url = 'data:application/json;base64,' + btoa(unescape(encodeURIComponent(result)));
+            chrome.downloads.download({
+                url: url,
+                filename: 'highlighter_export.json'
+            });
+        });
+    }
 });
