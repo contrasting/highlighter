a723f438ea1be30216d4222b649732c980ba227e
fredchen <fredchen@economist.com>
2024-08-02 15:27:01 +0100

    add import logic

--- src/import.js
+++ src/import.js
@@ -1,4 +1,8 @@
 // https://developer.chrome.com/docs/capabilities/web-apis/file-system-access
 async function importHighlights() {
     const fileHandles = await window.showOpenFilePicker();
+    const text = await (await fileHandles[0].getFile()).text()
+    const json = JSON.parse(text);
+    await chrome.storage.local.set(json);
+    console.log("Successfully imported");
 }
