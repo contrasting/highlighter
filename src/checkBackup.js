6a068375f34497171a1cb3042e6da4e3441cb328
fredchen <fredchen@economist.com>
2025-06-04 11:17:58 +0100

    fix: change to weekly reminder

--- src/checkBackup.js
+++ src/checkBackup.js
@@ -4,8 +4,8 @@ function checkBackup() {
     chrome.storage.sync.get(backupTimeKey, (result) => {
         if (result[backupTimeKey] != null) {
             const lastBackup = new Date(result[backupTimeKey]);
-            if (lastBackup.getTime() < new Date().getTime() - 1000 * 60 * 60 * 24) {
-                alert("Last backed up more than one day ago")
+            if (lastBackup.getTime() < new Date().getTime() - 1000 * 60 * 60 * 24 * 7) {
+                alert("Last backed up more than a week ago")
             }
         } else {
             alert("Last backed up date is null")
